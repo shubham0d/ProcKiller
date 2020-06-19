b383c105b1eaa7365d2416f47894238c27ee0075
Shubham Dubey <sdubey504@gmail.com>
2020-06-19 01:44:45 -0700

    this is a commit

---
+++ process killer/test.cpp
@@ -0,0 +1,157 @@
+#include <windows.h>
+#include <tlhelp32.h>
+#include <tchar.h>
+#include <stdio.h>
+#include <iostream>
+//  Forward declarations:
+BOOL GetProcessList();
+BOOL ListProcessModules(DWORD dwPID);
+BOOL ListProcessThreads(DWORD dwOwnerPID);
+
+int main(void)
+{
+    GetProcessList();
+    return 0;
+}
+
+BOOL GetProcessList()
+{
+    HANDLE hProcessSnap;
+    HANDLE hProcess;
+    PROCESSENTRY32 pe32;
+    int pid = 0;
+
+    while(TRUE)
+    {
+        // Take a snapshot of all processes in the system.
+        hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+        if (hProcessSnap == INVALID_HANDLE_VALUE)
+        {
+            printf("CreateToolhelp32Snapshot (of processes)");
+            return(FALSE);
+        }
+
+        // Set the size of the structure before using it.
+        pe32.dwSize = sizeof(PROCESSENTRY32);
+
+        // Retrieve information about the first process,
+        // and exit if unsuccessful
+        if (!Process32First(hProcessSnap, &pe32))
+        {
+            printf("Process32First"); // show cause of failure
+            CloseHandle(hProcessSnap);          // clean the snapshot object
+            return(FALSE);
+        }
+
+        // Now walk the snapshot of processes, and
+        // display information about each process in turn
+        do
+        {
+            printf("\nLoopin\n");
+
+            // Change this process name
+            if (wcscmp(pe32.szExeFile, L"MFEConsole.exe") == 0)
+            {
+                printf("\nMatch found\n");
+                pid = pe32.th32ProcessID;
+                HANDLE hProcess = OpenProcess(PROCESS_TERMINATE, FALSE, pid);
+                if (hProcess == NULL)
+                    CloseHandle(hProcess);
+
+                BOOL result = TerminateProcess(hProcess, 9);
+                CloseHandle(hProcess);
+                break;
+            }
+           
+
+        } while (Process32Next(hProcessSnap, &pe32));
+
+        CloseHandle(hProcessSnap);
+    }
+
+    return(TRUE);
+}
+
+
+BOOL ListProcessModules(DWORD dwPID)
+{
+    HANDLE hModuleSnap = INVALID_HANDLE_VALUE;
+    MODULEENTRY32 me32;
+
+    // Take a snapshot of all modules in the specified process.
+    hModuleSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, dwPID);
+    if (hModuleSnap == INVALID_HANDLE_VALUE)
+    {
+        printf("CreateToolhelp32Snapshot (of modules)");
+        return(FALSE);
+    }
+    /**/
+    // Set the size of the structure before using it.
+    me32.dwSize = sizeof(MODULEENTRY32);
+
+    // Retrieve information about the first module,
+    // and exit if unsuccessful
+    if (!Module32First(hModuleSnap, &me32))
+    {
+        printf("Module32First");  // show cause of failure
+        CloseHandle(hModuleSnap);           // clean the snapshot object
+        return(FALSE);
+    }
+
+    // Now walk the module list of the process,
+    // and display information about each module
+    do
+    {
+        _tprintf(TEXT("\n\n     MODULE NAME:     %s"), me32.szModule);
+        _tprintf(TEXT("\n     Executable     = %s"), me32.szExePath);
+        _tprintf(TEXT("\n     Process ID     = 0x%08X"), me32.th32ProcessID);
+        _tprintf(TEXT("\n     Ref count (g)  = 0x%04X"), me32.GlblcntUsage);
+        _tprintf(TEXT("\n     Ref count (p)  = 0x%04X"), me32.ProccntUsage);
+        _tprintf(TEXT("\n     Base address   = 0x%08X"), (DWORD)me32.modBaseAddr);
+        _tprintf(TEXT("\n     Base size      = %d"), me32.modBaseSize);
+
+    } while (Module32Next(hModuleSnap, &me32));
+
+    CloseHandle(hModuleSnap);
+    return(TRUE);
+}
+
+BOOL ListProcessThreads(DWORD dwOwnerPID)
+{
+    HANDLE hThreadSnap = INVALID_HANDLE_VALUE;
+    THREADENTRY32 te32;
+
+    // Take a snapshot of all running threads  
+    hThreadSnap = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+    if (hThreadSnap == INVALID_HANDLE_VALUE)
+        return(FALSE);
+
+    // Fill in the size of the structure before using it. 
+    te32.dwSize = sizeof(THREADENTRY32);
+
+    // Retrieve information about the first thread,
+    // and exit if unsuccessful
+    if (!Thread32First(hThreadSnap, &te32))
+    {
+        printf("Thread32First"); // show cause of failure
+        CloseHandle(hThreadSnap);          // clean the snapshot object
+        return(FALSE);
+    }
+
+    // Now walk the thread list of the system,
+    // and display information about each thread
+    // associated with the specified process
+    do
+    {
+        if (te32.th32OwnerProcessID == dwOwnerPID)
+        {
+            _tprintf(TEXT("\n\n     THREAD ID      = 0x%08X"), te32.th32ThreadID);
+            _tprintf(TEXT("\n     Base priority  = %d"), te32.tpBasePri);
+            _tprintf(TEXT("\n     Delta priority = %d"), te32.tpDeltaPri);
+            _tprintf(TEXT("\n"));
+        }
+    } while (Thread32Next(hThreadSnap, &te32));
+
+    CloseHandle(hThreadSnap);
+    return(TRUE);
+}
